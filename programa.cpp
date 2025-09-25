89e8328d332827f976e2c81a9a9aa11deba39b03
vingilytegabija-rgb <vingilytegabija@gmail.com>
2025-09-25 19:19:38 +0300

    Create programa.cpp
    Programa leidžia įvesti studento vardą bei pavardę naudojant string

---
+++ programa.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string vardas, pavarde;
+    cout << "Iveskite studento varda: ";
+    cin >> vardas;
+    cout << "Iveskite studento pavarde: ";
+    cin >> pavarde;
+    cout << "Studentas: " << vardas << " " << pavarde << endl;
+    return 0;
+}
