3f179fcc3043e4c5e19aae71a1f0448134a316b6
vingilytegabija-rgb <vingilytegabija@gmail.com>
2025-09-26 15:11:22 +0300

    Update programa.cpp
    Programa leidžia įrašyti neribotą skaičių studentų bei apskaičiuoja jų vidurkius

--- programa.cpp
+++ programa.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 #include <iomanip>
 
 using namespace std;
@@ -18,35 +19,52 @@ double skaicVid(const vector<int>& pazymiai) {
     for (int p : pazymiai) suma+= p;
     return (double)suma/pazymiai.size();
 }
-int main(){
-    Studentas s;
-    cout<<"Įveskite studento vardą:";
-    cin>>s.vardas;
-    cout<<"Įveskite studento pavardę:";
-    cin>>s.pavarde;
+int main() {
+    vector<Studentas> visiStudentai;
+    string eilute;
+
+    while(true) {
+        Studentas s;
+        cout<<"\n---Naujas studentas ---\n";
+
+        cout<<"Įveskite studento vardą (tuščia eilutė reiškia studentų sąrašo pabaigą):";
+        getline(cin, eilute);
+        if (eilute.empty()) break;
+        s.vardas = eilute;
+
+        cout<<"Įveskite studento pavardę:";
+        getline(cin, s.pavarde);
 
     cout<<"Įveskite namų darbų rezultatus(teigiami skaičiai rašomi be kablelių (formatas 4 5 2 0), baigiama su nuliu):";
+    string ndEilute;
+    getline(cin, ndEilute);
+    stringstream ss(ndEilute);
     int paz;
-    while(true){
-        cin>>paz;
+    while(ss>>paz){
         if(paz==0) break;
         s.nd.push_back(paz);
     }
+
 cout<<"Įveskite egzamino rezultatą:";
-cin>>s.egz;
+getline(cin, eilute);
+s.egz = stoi(eilute);
 
 s.nd.push_back(s.egz);
-double galutinis = skaicVid(s.nd);
+visiStudentai.push_back(s);
+    }
 
-cout<< endl;
+cout<< "\nRezultatai:\n";
 cout<< left<< setw(12) <<"Vardas"
      << setw(12) <<"Pavardė"
      << setw(20) <<"Galutinis(Vid.)"<<endl;
 cout << "---------------------------------------------" << endl;
 
-cout<<left<<setw(12)<<s.vardas
+for (Studentas s: visiStudentai){
+    double galutinis = skaicVid(s.nd);
+    cout<<left<<setw(12)<<s.vardas
     <<setw(12)<<s.pavarde
     <<setw(20) <<fixed<<setprecision(2)<<galutinis<<endl;
+}
 return 0;
 }
 
