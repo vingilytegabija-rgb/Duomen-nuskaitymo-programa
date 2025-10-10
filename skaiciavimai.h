ac02cd5defeb8ae3cf8bc775d531320e3f5ad750
vingilytegabija-rgb <vingilytegabija@gmail.com>
2025-10-10 20:10:55 +0300

    Create skaiciavimai.h
    pridetas failas skaiciavimai.h

---
+++ skaiciavimai.h
@@ -0,0 +1,10 @@
+#ifndef SKAICIAVIMAI_H
+#define SKAICIAVIMAI_H
+
+#include <vector>
+using namespace std;
+
+double skaicVid(const vector<int>& pazymiai);
+double skaicMediana(vector<int> pazymiai);
+
+#endif
