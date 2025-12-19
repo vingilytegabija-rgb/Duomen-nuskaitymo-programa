25db5369967056c66dfa5e3dfae69e0b7c9b4f8b
vingilytegabija-rgb <vingilytegabija@gmail.com>
2025-12-19 06:04:15 +0200

    patvarkytas failas

--- studentas.cpp
+++ studentas.cpp
@@ -1,39 +1,22 @@
 #include "studentas.h"
 #include <fstream>
-#include <cstdlib>
 #include <iomanip>
-#include <iostream>
-using namespace std;
+#include <chrono>
 
-Studentas generuotiStudenta(int id, int ndKiek) {
-    Studentas s;
-    s.vardas = "Vardas" + to_string(id);
-    s.pavarde = "Pavarde" + to_string(id);
-    for (int i = 0; i < ndKiek; i++)
-        s.nd.push_back(rand() % 11);
-    s.egz = rand() % 11;
-    return s;
-}
+void generuotiFaila(std::string vardas, int kiekis) {
+    std::ofstream f(vardas);
+    // Sugeneruojame antraštę
+    f << std::left << std::setw(20) << "Vardas" << std::setw(20) << "Pavarde";
+    for (int i = 1; i <= 5; i++) f << std::setw(5) << "ND" + std::to_string(i);
+    f << "Egz." << std::endl;
 
-void sugeneruotiFaila(int studentuKiekis, int ndKiek) {
-    string failoVardas = "studentai_" + to_string(studentuKiekis) + ".txt";
-    ofstream out(failoVardas);
-    if (!out) {
-        cerr << "Nepavyko sukurti failo: " << failoVardas << endl;
-        return;
+    for (int i = 1; i <= kiekis; i++) {
+        f << std::left << std::setw(20) << ("Vardas" + std::to_string(i))
+          << std::setw(20) << ("Pavarde" + std::to_string(i));
+        for (int j = 0; j < 6; j++) f << std::setw(5) << rand() % 11;
+        f << "\n";
     }
-
-    out << "Vardas Pavarde ";
-    for (int i = 1; i <= ndKiek; i++) out << "ND" << i << " ";
-    out << "Egzaminas\n";
-
-    for (int i = 1; i <= studentuKiekis; i++) {
-        Studentas s = generuotiStudenta(i, ndKiek);
-        out << s.vardas << " " << s.pavarde << " ";
-        for (int balas : s.nd) out << balas << " ";
-        out << s.egz << endl;
-    }
-
-    out.close();
-    cout << "Failas " << failoVardas << " sukurtas su " << studentuKiekis << " įrašų.\n";
+    f.close();
+    std::cout << "Failas " << vardas << " sukurtas.\n";
 }
+    }
