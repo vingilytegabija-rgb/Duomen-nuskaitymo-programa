88169428e027a945a6b72bcf1e8f5158dc7eb304
vingilytegabija-rgb <vingilytegabija@gmail.com>
2025-12-19 07:22:16 +0200

    patvarkytas kodas

--- studentai.cpp
+++ studentai.cpp
@@ -19,7 +19,6 @@ void rusiuotiIrIrasyti(T& visi, string failoVardas, string konteineris) {
     }
     auto e2 = high_resolution_clock::now();
 
-    // Matuojame tik įrašymą į failus
     auto s3 = high_resolution_clock::now();
     ofstream fV(konteineris + "_vargsai_" + failoVardas);
     ofstream fK(konteineris + "_kietiakai_" + failoVardas);
@@ -33,6 +32,54 @@ void rusiuotiIrIrasyti(T& visi, string failoVardas, string konteineris) {
     fK.close();
     auto e3 = high_resolution_clock::now();
 
-    cout << konteineris << " rusiavimas uztruko: " << duration<double>(e2 - s2).count() << " s\n";
-    cout << konteineris << " irasymas uztruko:  " << duration<double>(e3 - s3).count() << " s\n";
+    cout << konteineris << " rusiavimas: " << duration<double>(e2 - s2).count() << " s\n";
+    cout << konteineris << " irasymas:  " << duration<double>(e3 - s3).count() << " s\n";
+}
+
+void generuotiFaila(string vardas, int kiekis) {
+    auto t1 = high_resolution_clock::now();
+    ofstream f(vardas);
+    f << left << setw(20) << "Vardas" << setw(20) << "Pavarde";
+    for (int i = 1; i <= 5; i++) f << setw(5) << "ND" + to_string(i);
+    f << "Egz." << endl;
+
+    for (int i = 1; i <= kiekis; i++) {
+        f << left << setw(20) << ("Vardas" + to_string(i)) << setw(20) << ("Pavarde" + to_string(i));
+        for (int j = 0; j < 6; j++) f << setw(5) << rand() % 11;
+        f << "\n";
+    }
+    f.close();
+    auto t2 = high_resolution_clock::now();
+    cout << "Failas " << vardas << " sukurtas per " << duration<double>(t2 - t1).count() << " s\n";
+}
+
+void matuotiSpartaVector(string failoVardas) {
+    auto s1 = high_resolution_clock::now();
+    vector<Studentas> visi;
+    ifstream in(failoVardas);
+    if (!in) return;
+
+    string eilute;
+    getline(in, eilute);
+    while (getline(in, eilute)) {
+        stringstream ss(eilute);
+        Studentas s;
+        ss >> s.vardas >> s.pavarde;
+        int p;
+        while (ss >> p) s.nd.push_back(p);
+        
+        if (!s.nd.empty()) {
+            s.egz = s.nd.back();
+            s.nd.pop_back();
+            s.galutinis = 0.4 * skaicVid(s.nd) + 0.6 * s.egz;
+            visi.push_back(s);
+        }
+    }
+    in.close();
+    auto e1 = high_resolution_clock::now();
+
+    cout << "\n--- VECTOR Analize (" << failoVardas << ") ---\n";
+    cout << "Nuskaitymas: " << duration<double>(e1 - s1).count() << " s\n";
+    
+    rusiuotiIrIrasyti(visi, failoVardas, "Vector");
 }
