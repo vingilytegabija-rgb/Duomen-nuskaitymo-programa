871b18ba56d2ccb62ff1301112ea2e789b60c097
vingilytegabija-rgb <vingilytegabija@gmail.com>
2025-12-19 07:02:28 +0200

    patvarkytas kodas

--- studentas.h
+++ studentas.h
@@ -1,5 +1,4 @@
 #ifndef STUDENTAS_H
-#d#ifndef STUDENTAS_H
 #define STUDENTAS_H
 
 #include <iostream>
@@ -11,11 +10,13 @@ struct Studentas {
     std::string pavarde;
     std::vector<int> nd;
     int egz;
+    double galutinis;
 };
 
 double skaicVid(const std::vector<int>& pazymiai);
 double skaicMediana(std::vector<int> pazymiai);
-
 void generuotiFaila(std::string vardas, int kiekis);
+void matuotiSparta(std::string failoVardas);
+void rusiuotiIrIrasyti(std::vector<Studentas>& visi, std::string failoVardas);
 
 #endif
