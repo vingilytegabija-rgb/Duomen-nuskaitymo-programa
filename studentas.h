e4952a04741813036b2d2065dc2294ea85a07a8e
vingilytegabija-rgb <vingilytegabija@gmail.com>
2025-12-19 07:55:40 +0200

    Add new strategy functions for student processing

--- studentas.h
+++ studentas.h
@@ -5,6 +5,10 @@
 #include <vector>
 #include <list>
 #include <string>
+#include <algorithm>
+#include <chrono>
+#include <fstream>
+#include <iomanip>
 
 struct Studentas {
     std::string vardas;
@@ -17,8 +21,17 @@ struct Studentas {
 double skaicVid(const std::vector<int>& pazymiai);
 double skaicMediana(std::vector<int> pazymiai);
 void generuotiFaila(std::string vardas, int kiekis);
+bool arVargsiukas(const Studentas& s); // Reikalinga skirstymui
 
 void matuotiSpartaVector(std::string failoVardas);
 void matuotiSpartaList(std::string failoVardas);
+
+
+void strategija1(std::vector<Studentas>& bendras, std::vector<Studentas>& vargsiukai, std::vector<Studentas>& kietiakai);
+
+void strategija2(std::vector<Studentas>& kietiakai, std::vector<Studentas>& vargsiukai);
+void strategija2List(std::list<Studentas>& kietiakai, std::list<Studentas>& vargsiukai);
+
+void strategija3(std::vector<Studentas>& kietiakai, std::vector<Studentas>& vargsiukai);
 
 #endif
