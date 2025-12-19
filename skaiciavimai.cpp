84ee5e7de83c64b3d5a2d40a1ce802806b3fb72e
vingilytegabija-rgb <vingilytegabija@gmail.com>
2025-12-19 06:03:17 +0200

    patvarkytas failas

--- skaiciavimai.cpp
+++ skaiciavimai.cpp
@@ -1,19 +1,17 @@
-#include "skaiciavimai.h"
+#include "studentas.h"
 #include <algorithm>
 
-double skaicVid(const vector<int>& pazymiai) {
+double skaicVid(const std::vector<int>& pazymiai) {
     if (pazymiai.empty()) return 0;
     int suma = 0;
     for (int p : pazymiai) suma += p;
     return (double)suma / pazymiai.size();
 }
 
-double skaicMediana(vector<int> pazymiai) {
+double skaicMediana(std::vector<int> pazymiai) {
     if (pazymiai.empty()) return 0;
-    sort(pazymiai.begin(), pazymiai.end());
+    std::sort(pazymiai.begin(), pazymiai.end());
     int n = pazymiai.size();
-    if (n % 2 == 0)
-        return (pazymiai[n / 2 - 1] + pazymiai[n / 2]) / 2.0;
-    else
-        return pazymiai[n / 2];
+    if (n % 2 == 0) return (pazymiai[n / 2 - 1] + pazymiai[n / 2]) / 2.0;
+    else return pazymiai[n / 2];
 }
